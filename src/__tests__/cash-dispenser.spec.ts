485f809c423b649f71678d8f79f8351f18a42b5b
Florent Le Gall <florent.legall@fluo.com>
2017-07-25 14:15:04 +0200

    Add scenario text

--- src/__tests__/cash-dispenser.spec.ts
+++ src/__tests__/cash-dispenser.spec.ts
@@ -1,5 +1,16 @@
 import { CashDispenser, Card, Account } from '../cash-dispenser';
 
+/*
+    Scenario 1: Account is in credit+
+    Given the account is in credit
+        And the card is valid
+        And the dispenser contains cash
+    When the customer requests cash
+    Then ensure the account is debited
+        And ensure cash is dispensed    
+        And ensure the card is returned
+*/
+
 describe('cash dispenser', () => {
     it('should dispense cash when in credit +', () => {
         const account = new Account(100);
